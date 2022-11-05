2b731fb2aa02ff37cf06f78b22059c621e60eccd
Qi Zeng <ivan@foxmail.com>
2022-11-05 23:36:29 +0000

    Deploy success

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -14,10 +14,10 @@ class DbConfig{
         logger.info{"Starting DB Connection..."}
 
         val dbConfig = Database.connect(
-            "jdbc:postgresql://ec2-54-147-33-38.compute-1.amazonaws.com:5432/dd1alnffpluckj?sslmode=require",
+            "jdbc:postgresql://ec2-44-195-132-31.compute-1.amazonaws.com:5432/dcq2270vico6cm?sslmode=require",
             driver = "org.postgresql.Driver",
-            user = "decoqpkgvcdpue",
-            password = "c03e409c7da188b020d3be9238b88e61fa4b4fb4ecd0d6551523dec6daf1d78a")
+            user = "hpdevxvinhxkvh",
+            password = "e1bb5ee95437553473e02c5776b211f8954394a5d498992f98b2e6fbc15d9523")
 
         logger.info{"DbConfig name = " + dbConfig.name}
         logger.info{"DbConfig url = " + dbConfig.url}
