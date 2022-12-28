4eed0319fd41f32dbf465f258c819037db39366d
Qi Zeng <ivan@foxmail.com>
2022-12-28 02:02:09 +0000

    Connecting to ElephantSQL

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -11,16 +11,25 @@ class DbConfig{
     //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
     fun getDbConnection() :Database{
 
+        val logger = KotlinLogging.logger {}
         logger.info{"Starting DB Connection..."}
 
-        val dbConfig = Database.connect(
-            "jdbc:postgresql://ec2-44-195-132-31.compute-1.amazonaws.com:5432/dcq2270vico6cm?sslmode=require",
-            driver = "org.postgresql.Driver",
-            user = "hpdevxvinhxkvh",
-            password = "e1bb5ee95437553473e02c5776b211f8954394a5d498992f98b2e6fbc15d9523")
+        val PGUSER = "qykpvqud"
+        val PGPASSWORD = "MOgVIoeqO8Ovwam-7A_lDkzm5WfPrO5o"
+        val PGHOST = "mel.db.elephantsql.com/qykpvqud"
+        val PGPORT = "5432"
+        val PGDATABASE = "qykpvqud"
 
-        logger.info{"DbConfig name = " + dbConfig.name}
-        logger.info{"DbConfig url = " + dbConfig.url}
+        //url format should be jdbc:postgresql://host:port/database
+        val url = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+
+        val dbConfig = Database.connect(url,
+            driver="org.postgresql.Driver",
+            user = PGUSER,
+            password = PGPASSWORD
+        )
+
+        logger.info{"db url - connection: " + dbConfig.url}
 
         return dbConfig
     }
